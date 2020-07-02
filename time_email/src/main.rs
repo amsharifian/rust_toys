05040240bd593144d9b4e0f273973e01a81d156f
Amirali Sharifian <am.sharifian@gmail.com>
2020-07-02 12:03:25 -0700

    Add first toy example that send email periodically

---
+++ time_email/src/main.rs
@@ -0,0 +1,81 @@
+extern crate chrono;
+extern crate lettre;
+extern crate lettre_email;
+extern crate mime;
+
+use chrono::prelude::*;
+use chrono::Duration;
+use std::thread::sleep;
+use std::time;
+
+use lettre::{SmtpClient, Transport};
+use lettre_email::Email;
+use std::path::Path;
+
+use lettre::smtp::authentication::IntoCredentials;
+
+
+fn test(){
+    println!("This is a test!");
+}
+
+
+fn sendEmail(){
+
+    let smtp_address = "smtp.gmail.com";
+    let user_name = "am.sharifian@gmail.com";
+    let pass = "fujaibuoymyjruyg";
+
+    let email = Email::builder()
+    // Addresses can be specified by the tuple (email, alias)
+    .to(("amiralis@sfu.ca", "Amirali Sharifian"))
+    // ... or by an address only
+    .from((user_name, "Amirali Sharifian"))
+    .subject("Test Email")
+    .text("Hello world.")
+    .build()
+    .unwrap();
+
+    let credentials = (user_name, pass).into_credentials();
+
+    // Open a local connection on port 25
+    // let mut mailer = SmtpClient::new_unencrypted_localhost().unwrap().transport();
+    let mut client = SmtpClient::new_simple(smtp_address)
+    .unwrap()
+    .credentials(credentials)
+    .transport();
+
+    // Send the email
+    let result = client.send(email.into());
+
+    if result.is_ok() {
+    println!("Email sent");
+    } else {
+    println!("Could not send email: {:?}", result);
+    }
+
+    assert!(result.is_ok());
+}
+
+fn main() {
+    let local: DateTime<Local> = Local::now(); // e.g. `2014-11-28T21:45:59.324310806+09:00`
+    let tomorrow = local + Duration::days(1);
+
+    // println!("Hour: {}", local.time().hour12().1);
+    println!("Today: {}", local.to_string());
+    println!("Tomorrow: {}", tomorrow.to_string());
+
+    let mut cnt = 0;
+    sendEmail();
+    // loop{
+    //     sleep(time::Duration::new(2,0));
+    //     if cnt == 5 {   
+    //         break;
+    //     }
+    //     else{
+    //         test();
+    //         cnt += 1;
+    //     }
+    // }
+
+}
